9c05091c8e29d88e01e57cc96c200ffe1557947c
John <jschriem@gmail.com>
2019-03-29 20:16:30 -0700

    web.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 #Python library for accessing the Twitter API
 import tweepy
 
-#requests preforms http requests
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
-#BeautifulSoup handles HTML processing
-from bs4 import BeautifulSoup
-
 def mainFunction():
     consumer_key = 'nvNfiuecj0XCfgW1uaNkqR0kC'
     consumer_secret = 'H3MobEXiwTOZvUUPCrsP17zDUJBGyIJKlLzW9bF18qdDCrMYHB'
@@ -25,42 +18,6 @@ def mainFunction():
     #Auto Follow back any users that follow Tennis_Scores
     for follower in tweepy.Cursor(api.followers).items():
         follower.follow()
+        print(follower)
 
     #api.update_status("The awesome text you would like to tweet")
-
-
-def simple_get(url):
-
-    #Attempts to get the content at `url` by making an HTTP GET request.
-    #If the content-type of response is some kind of HTML/XML, return the
-    #text content, otherwise return None.
-
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
-
-
-def is_good_response(resp):
-
-    #Returns True if the response seems to be HTML, False otherwise.
-
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200
-            and content_type is not None
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    """
-    It is always a good idea to log errors.
-    This function just prints them, but you can
-    make it do anything.
-    """
-    print(e)
