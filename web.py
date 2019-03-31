a566a753b3f8093931e1ff5e981d3a188b754d15
John <jschriem@gmail.com>
2019-03-30 17:31:58 -0700

    Score extraction

--- web.py
+++ web.py
@@ -3,9 +3,45 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 #BeautifulSoup handles HTML processing
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+
+from urllib.request import urlopen #this module lets us grab web pages.
+#note the would be just 'from urllib' in python 2.7
+from bs4 import BeautifulSoup #this module lets us easily parse the html.
+#if you don't have BeautifulSoup downloaded, type 'pip install beautifulsoup4' into the command line
 
 
+tennis_ref = 'http://m.espn.com/general/tennis/dailyresults?wjb'
+
+score_url = urlopen(tennis_ref)
+#lets look at what is stored here:
+print(score_url)
+
+
+
+
+tennis_score = BeautifulSoup(score_url, 'html.parser')
+#game_section = tennis_score.find(class_="ind")
+game_section = tennis_score.findAll("div", class_="ind")
+#todays_games = game_section.findAll('',{'class','ind'})
+#print(tennis_score)
+#print(todays_games)
+#print(game_section)
+for line in game_section:
+   if line.find(string = "Final"):
+       print(line.text)
+           #if line.find(string = "("):
+
+
+
+#final = game_section.find(string = "Final")
+#print(final)
+
+
+
+
+
+"""
 def simple_get(url):
     #Attempts to get the content at `url` by making an HTTP GET request.
     #If the content-type of response is some kind of HTML/XML, return the
@@ -35,9 +71,10 @@ def is_good_response(resp):
 
 
 def log_error(e):
-    """
+
     It is always a good idea to log errors.
     This function just prints them, but you can
     make it do anything.
-    """
+
     print(e)
+"""
