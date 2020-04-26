4b0e7f27dc3b6dc1a1e9d6073624face9aacbec2
John <jschriem@gmail.com>
2020-04-25 18:40:03 -0700

    tennis-tweets revival

--- main.py
+++ main.py
@@ -5,40 +5,25 @@ Date: Created on March 24, 2019
 License: MIT License
 Description: Webscraping Twitter bot for Men's ATP tennis score retrieval.
 """
-
+# pylint: disable=import-error
+import time
 import tweepy
 import schedule
-import time
 from web import get_score
+
+#Twitter developer keys. See readme.md for more info
+
 
 
 
 def score_tweet():
-    get_score(auth,api)
-
-
-#Twitter developer keys. See readme.md for more info
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-auth = tweepy.OAuthHandler('', '')
-auth.set_access_token('', '')
-api = tweepy.API(auth)
-
-
-#prints out Tennis_Scores
-user = api.me()
-print (user.name)
-
-#Auto Follow back any users that follow Tennis_Scores
-for follower in tweepy.Cursor(api.followers).items():
-#  if() #follower is NOT alreadey been followed by me,
-   follower.follow()
+    """Get and post the tennis scores"""
+    user = API.me()
+    print(user.name)
+    get_score(AUTH, API)
 
 
 score_tweet()
-
 #Automated posting at 11:59pm each day
 #schedule.every().day.at("23:59").do(score_tweet)
 #while True:
--- web.py
+++ web.py
@@ -5,22 +5,18 @@ Date: Created on March 24, 2019
 License: MIT License
 Description: Webscraping Twitter bot for Men's ATP tennis score retrieval.
 """
-
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
+# pylint: disable=import-error
+import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
-import tweepy
 
-#Main function to parse through html to find tennis data
+
 def get_score(auth, api):
+    """Main function to parse through html to find tennis data"""
 
     #Website url for webscraping
-    tennis_ref = 'http://m.espn.com/general/tennis/dailyresults?wjb'
+    tennis_ref = 'http://m.espn.com/general/tennis/dailyresults?date=20200125&matchType=1&wjb='
     score_url = urlopen(tennis_ref)
-
 
     #Parse through site's html, and find class 'ind' (results stored here)
     tennis_score = BeautifulSoup(score_url, 'html.parser')
@@ -39,16 +35,22 @@ def get_score(auth, api):
 
     #get the round of tournament
     round_section = tennis_score.findAll("div", class_="ind sub bold")
+    fullround = ""
     for line in round_section:
-     if line.text != "FULL TOURNAMENT RESULTS":
-         fullround = line.text
+        if line.text != "FULL TOURNAMENT RESULTS":
+            fullround = line.text
 
     #Seperates daily results into a list
     matches = []
     for line in game_section:
-        if line.find(string = "Final"):
+        print(line)
+        print(line.text.find("Final"))
+        if (line.text.find("Final") >= 0):
+            print("check!")
+            print(line)
             word = line.text.split("Final")
             matches = matches + word
+    print(matches)
 
     count_match(matches, auth, api, tournament_tag, fullround)
 
@@ -56,7 +58,7 @@ def get_score(auth, api):
 def count_match(matches, auth, api, tournament_tag, fullround):
     count = 0
     for i in range(len(matches)):
-        if matches[i]  != "":
+        if matches[i] != "":
             count = count + 1
     #print(count + " matches today")
     final_score(matches, count, auth, api, tournament_tag, fullround)
@@ -64,10 +66,10 @@ def count_match(matches, auth, api, tournament_tag, fullround):
 #Splits lists results into strings and adds a space betwen score and players
 def final_score(matches, count, auth, api, tournament_tag, fullround):
     for i in range(len(matches)):
-        if matches[i]  != "":
+        if matches[i] != "" and matches[i] != "FULL TOURNAMENT RESULTS" and matches[i] != "TENNIS HOME PAGE":
             score = re.sub(r'([a-z])(\d{1})', r'\1 \2', matches[i])
             player = re.findall(r'([A-Z]\w+-*\w*)', score)
             round = re.findall(r'^[^:]+', fullround)
-
-
-            api.update_status(round[0] + " of the #" + tournament_tag + ": " +score + " #tennis #atp" + " #" + player[0] + " #" + player[1])
+            if(len(player)>1):
+                print(round[0] + " of the #" + tournament_tag + ": " +score + " #tennis #atp" + " #" + player[0] + " #" + player[1])
+            #api.update_status(round[0] + " of the #" + tournament_tag + ": " +score + " #tennis #atp" + " #" + player[0] + " #" + player[1])
